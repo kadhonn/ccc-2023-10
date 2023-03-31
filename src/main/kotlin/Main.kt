c99086a7ec5bd5c06d3d76bf766286d83a09e15c
Martin Weber <martin.weber@teamecho.at>
2023-03-31 15:32:45 +0200

    level 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,50 @@
 import Utils.Companion.fileForExample
 import java.io.File
+import java.util.stream.IntStream.range
 
-val CURRENT_LEVEL = "level1"
+val CURRENT_LEVEL = "level2"
 
 fun compute(inputFile: File, outputFile: File) {
 
-    var n = 0
+//    var n = 0
     val lines = inputFile.readLines();
 
-    outputFile.writeText("18")
+    val tournamentCount = lines[0].split(" ")[0].toInt()
+    val fighterCount = lines[0].split(" ")[1].toInt()
+//    var n = lines[0].toInt()
+
+    val roundsPerLine = 2
+
+    val tournamentLines = lines.subList(1, lines.size)
+    var result = ""
+
+    for(tournament in tournamentLines) {
+        var currentRound = tournament
+        for(rount in range(0, roundsPerLine)) {
+            currentRound = currentRound.windowed(2, 2).map {
+                determineFightWinner(it)
+            }.joinToString("")
+        }
+        result += currentRound + "\n"
+    }
+
+//    for (i in range(0, n)) {
+//        val fight = lines[1 + i]
+//
+//        result += determineFightWinner(fight)
+//        result += "\n"
+//    }
+    outputFile.writeText(result.replace("\n", "\r\n"))
+}
+
+private fun determineFightWinner(fight: String) = if (fight.contains('R') && fight.contains('P')) {
+    "P"
+} else if (fight.contains('S') && fight.contains('P')) {
+    "S"
+} else if (fight.contains('R') && fight.contains('S')) {
+    "R"
+} else {
+    fight[0].toString()
 }
 
 fun main() {
