42a3d0cacba81143ed667df50a1f16f21cbd9da9
Martin Weber <martin.weber@teamecho.at>
2023-10-20 15:26:32 +0200

    level1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,183 +1,39 @@
 import Utils.Companion.fileForExample
 import java.io.File
+import java.util.Scanner
 import java.util.stream.IntStream.range
 
-val CURRENT_LEVEL = "level3"
+val CURRENT_LEVEL = "level1"
 
 fun compute(inputFile: File, outputFile: File) {
 
-//    var n = 0
-    val lines = inputFile.readLines();
+    val scanner = Scanner(inputFile.inputStream())
 
-    val tournamentCount = lines[0].split(" ")[0].toInt()
-    val fighterCount = lines[0].split(" ")[1].toInt()
-//    var n = lines[0].toInt()
+    val mapSize = scanner.nextInt()
+    scanner.nextLine();
 
-    val roundsPerLine = 2
+    val map = mutableMapOf<Vector2D,Char>()
+    for(i in 0 until mapSize) {
+        val currentLine = scanner.nextLine()
+        check(currentLine.length == mapSize)
 
-    val tournamentLines = lines.subList(1, lines.size)
+        for(j in 0 until mapSize) {
+            val currentPos = Vector2D(j,i)
+            map[currentPos] = currentLine[j]
+        }
+    }
+
+    val coordinateCount = scanner.nextInt()
+    scanner.nextLine()
     var result = ""
 
-    for(tournament in tournamentLines) {
-        val fighterPairs = tournament.split(" ")
-        check(fighterPairs.size <= 3)
-        val fighterMap =
-            fighterPairs.associate { it[it.length - 1] to it.substring(0, it.length - 1).toInt() }.toMutableMap()
-
-        var pool = Pool(fighterMap, fighterCount)
-        pool.getPair("SP")
-        val canAssureSPreachesAndWinsRound2 = pool.getPair("PR") || pool.getPair("PP")
-        check(canAssureSPreachesAndWinsRound2) { " Cant assure" }
-
-        while (!pool.getTournamentFull()) {
-            var testPool = pool.copy()
-            if ((testPool.getPair("RR")) && (testPool.getPair("PR") || testPool.getPair("PP"))) {
-                pool = testPool
-                continue
-            }
-            testPool = pool.copy()
-            if ((testPool.getPair("RS")) && (testPool.getPair("PR") || testPool.getPair("PP"))) {
-                pool = testPool
-                continue
-            }
-            else
-            {
-                pool = pool.get2PairsFrom(setOf("PR", "SS", "PP", "SP"))
-                continue;
-            }
-//            else if (setOf("PR", "SS", "PP", "SP").any {
-//                    val testPool2 = testPool.copy()
-//                    val a = testPool2.getPair(it)
-//                    val b = setOf("PR", "SS", "PP", "SP").any {
-//                        testPool2.getPair(it)
-//                    }
-//                    if (a && b) {
-//                        testPool = testPool2
-//                        true
-//                    } else
-//                        false
-//                })
-//            else if((testPool.getPair("PR") || testPool.getPair("SS") || testPool.getPair("PP") || testPool.getPair("SP") ) && (testPool.getPair("PR")  || testPool.getPair("SS") ||testPool.getPair("PP") || testPool.getPair("SP")))
-//            {
-//                pool = testPool
-//                continue
-//            } else {
-//                println("dont know")
-//            }
-        }
-        check(validateSolution(pool.tournament))
-        result += pool.tournament + "\n"
-
-//        fighterMap['S'] = fighterMap.getValue('S') - 1
-//        fighterMap['P'] = fighterMap.getValue('P') - 1
-
-//        var myTournament = "SP"
-
-
-//        var currentRound = tournament
-//        for (rount in range(0, roundsPerLine)) {
-//            currentRound = currentRound.windowed(2, 2).map {
-//                determineFightWinner(it)
-//            }.joinToString("")
-//        }
-//        result += currentRound + "\n"
+    for(i in 0  until coordinateCount) {
+        val currentCoordinate = Vector2D.fromString(scanner.nextLine())
+        val tileType = map.getValue(currentCoordinate)
+        result = result + "\n" + tileType;
     }
-
-    outputFile.writeText(result.replace("\n", "\r\n"))
-}
-
-data class Pool(var fighterMap: MutableMap<Char, Int>, val tournamentSize: Int, var tournament: String = "") {
-
-    fun getTournamentFull(): Boolean {
-        check(tournament.length <= tournamentSize)
-        return tournament.length == tournamentSize
-    }
-
-    fun testPair(pair: String): Boolean {
-        val tmp = fighterMap.toMutableMap()
-        for (c in pair) {
-            val cnt = tmp[c] ?: 0
-            if (cnt <= 0) {
-                return false;
-            }
-            tmp[c] = cnt - 1
-        }
-        return true;
-    }
-    fun get2PairsFrom(possible: Set<String>) : Pool
-    {
-        val new = possible.map {
-            val tmp = this.copy()
-            val a = tmp.getPair(it)
-            if(!a) {
-                null
-            }
-            else {
-                val b = possible.any { tmp.getPair(it) }
-                if(!b){
-                    null
-                }
-                else
-                {
-                    tmp
-                }
-            }
-        }.filterNotNull()
-
-
-        check(new.size >= 1) {
-
-            println("here") }
-        return new.first()
-    }
-
-    fun getPair(pair: String): Boolean {
-        val tmp = fighterMap.toMutableMap()
-        for (c in pair) {
-            val cnt = tmp[c] ?: 0
-            if (cnt <= 0) {
-                return false;
-            }
-            tmp[c] = cnt - 1
-        }
-        fighterMap = tmp;
-        tournament += pair
-        return true;
-    }
-
-    fun getPairs(pair: String, cnt: Int): Int {
-        for (i in range(0, cnt)) {
-            if (!getPair(pair)) {
-                return i
-            }
-        }
-        return cnt;
-    }
-}
-
-private fun validateSolution(tournament: String): Boolean {
-    val roundsPerLine = 2
-    var currentRound = tournament
-    for (rount in range(0, roundsPerLine)) {
-        currentRound = currentRound.windowed(2, 2).map {
-            determineFightWinner(it)
-        }.joinToString("")
-    }
-    return !currentRound.contains("R") && currentRound.contains("S")
-}
-
-private fun determineFightWinner(fight: String) = if (fight.contains('R') && fight.contains('P')) {
-    "P"
-} else if (fight.contains('S') && fight.contains('P')) {
-    "S"
-} else if (fight.contains('R') && fight.contains('S')) {
-    "R"
-} else {
-    fight[0].toString()
-}
-
-fun main() {
-    Utils.testFile(fileForExample("example"))
+    val finalResult = result.trim().replace("\n", "\r\n") + "\r\n"
+    outputFile.writeText(finalResult)
 }
 
 
@@ -206,5 +62,11 @@ data class Vector2D(val x: Int, val y: Int) {
         val DOWN = Vector2D(0, 1)
 
         val allDirections = setOf(ZERO, LEFT, UP, RIGHT, DOWN)
+
+        fun fromString(str : String) : Vector2D {
+            val parts = str.split(",").map { it.toInt() }
+            check(parts.size == 2)
+            return Vector2D(parts[0], parts[1])
+        }
     }
 }
